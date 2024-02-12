173d51b443353dafdb206fc9139808de412d95b6
Annadvi Jundi <144420937+AnnadviJundi@users.noreply.github.com>
2024-02-12 16:10:14 +0700

    Update MahasiswaController.php

--- MahasiswaController.php
+++ MahasiswaController.php
@@ -7,8 +7,8 @@
             $mhs1 = 'Fawwaz'; $asal1 = 'Jakarta';
             $mhs2 = 'Inaya'; $asal2 = 'Depok';
             $mhs3 = 'Zidan'; $asal3 = 'Binjai';
-            $mhs4 = 'Bamabang'; $asal4 = 'Mojokerto';
-            $mhs5 = 'Bamabang'; $asal5 = 'Mojokerto';
+            $mhs4 = 'Bambang'; $asal4 = 'Mojokerto';
+            $mhs5 = 'Bambang'; $asal5 = 'Mojokerto';
             return view('data_mahasiswa',
                 compact('mhs1','mhs2','mhs3','mhs4','mhs5','asal1','asal2','asal3','asal4','asal5')
             );
