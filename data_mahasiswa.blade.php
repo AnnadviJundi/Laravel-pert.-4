39e311fb372ff492807f97ff35d7ebfee1d6bc0d
Annadvi Jundi <144420937+AnnadviJundi@users.noreply.github.com>
2024-02-12 11:29:01 +0700

    Create data_mahasiswa.blade.php

---
+++ data_mahasiswa.blade.php
@@ -0,0 +1,32 @@
+<table border="1">
+  <tr>
+    <th align="center">Nomor</th>
+    <th>Nama Mahasiswa</th>
+    <th>Alamat</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>{{ $mhs1 }}</td>
+    <td>{{ $asal1 }}</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>{{ $mhs2 }}</td>
+    <td>{{ $asal2 }}</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>{{ $mhs3 }}</td>
+    <td>{{ $asal3 }}</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>{{ $mhs4 }}</td>
+    <td>{{ $asal4 }}</td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td>{{ $mhs5 }}</td>
+    <td>{{ $asal5 }}</td>
+  </tr>
+</table>
