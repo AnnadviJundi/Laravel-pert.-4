558f3a91aa385e0733f895e8bcc328f4ae6fd1d6
Annadvi Jundi <144420937+AnnadviJundi@users.noreply.github.com>
2024-02-12 14:36:00 +0700

    Update web.php

--- web.php
+++ web.php
@@ -1,1 +1,9 @@
-w
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Menambahkan view daftar_nilai
+//Tambahkan route baru di routes/web.php
+Route::get('/daftarnilai', function () {
+    return view('/p4/daftar_nilai');
+});
